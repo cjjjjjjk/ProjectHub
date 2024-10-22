f2dbeb7aeb7cf906f510ba047f38ee21be18efc4
NXTruongdk <truongnguyen31032004@gmail.com>
2024-10-22 10:17:47 +0700

    tuan

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPass, setShowPass] = useState(false);
+
+  const handleShowPass = () => {
+    setShowPass(!showPass);
+  };
+  const handleLogin = (e) => {
+    const account = {
+      email: email,
+      password: password,
+    };
+    console.log(account);
+  };
+
   return (
     <div className="h-[100vh]  text-white bg-violet-300 flex justify-center items-center">
       <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
@@ -13,18 +32,22 @@ const Login = () => {
             <input
               type="email"
               className="block w-72 py-2.3 px-0 text-lg text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
+
             <label
               htmlFor=""
               className="absolute text-lg text-white duration-300 transform -translate-y-5 scale-75 top-4  -z-10 origin-[0] peer-focus:left peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75"
             >
               Email
             </label>
+            <MdEmail className="absolute right-3 top-4 size-6"></MdEmail>
           </div>
           <div className="relative my-8 p-2 pt-4 border-2 border-black rounded-lg">
             <input
-              type="password"
+              type={showPass ? "text" : "password"}
               className="block w-72 py-2.3 px-0 text-lg text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
             <label
               htmlFor=""
@@ -32,6 +55,17 @@ const Login = () => {
             >
               Password
             </label>
+            <button
+              type="button"
+              className="absolute right-3 top-4"
+              onClick={handleShowPass}
+            >
+              {showPass ? (
+                <FaEye className="size-6" />
+              ) : (
+                <FaEyeSlash className="size-6" />
+              )}
+            </button>
           </div>
           <div className="flex justify-between items-center ">
             <div className="flex">
@@ -42,10 +76,10 @@ const Login = () => {
               Forgot Password?
             </Link>
           </div>
-
           <button
             type="submit"
-            className="w-full mb-4 py-2 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white "
+            className="w-full mb-4 py-2 text-[18px] mt-6 rounded-full bg-white text-black hover:bg-black hover:text-white"
+            onClick={handleLogin}
           >
             Login
           </button>
--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaRepeat } from "react-icons/fa6";
+
 const Register = () => {
   return (
     <div className="h-[100vh]  text-white bg-violet-300 flex justify-center items-center">
@@ -19,6 +23,7 @@ const Register = () => {
             >
               Email
             </label>
+            <MdEmail className="absolute right-3 top-4 size-6"></MdEmail>
           </div>
 
           <div className="relative my-4 p-2 pt-4 border border-black rounded-lg hover:border-amber-500">
@@ -32,6 +37,7 @@ const Register = () => {
             >
               Password
             </label>
+            <RiLockPasswordFill className="absolute right-3 top-4 size-6"></RiLockPasswordFill>
           </div>
 
           <div className="relative my-4 p-2 pt-4 border border-black rounded-lg hover:border-amber-500">
@@ -45,11 +51,12 @@ const Register = () => {
             >
               Confirm password
             </label>
+            <FaRepeat className="absolute right-3 top-4 size-5" />
           </div>
 
           <button
             type="submit"
-            className="w-full mb-4 py-2 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white "
+            className="w-full mb-4 py-2 text-[18px] mt-6 rounded-full bg-white text-black hover:bg-black hover:text-white "
           >
             Register
           </button>
