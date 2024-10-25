68f6b2863c1ccb7a28a32252a29420b81e37cd54
Đồng Văn Hiếu <hieu.dv224980@sis.hust.edu.vn>
2024-10-25 12:41:15 +0700

    adjust index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+const bodyParser = require("body-parser");
+const projectRoutes = require("./routes/projects");
+const errorHandler = require("./middleware/errorHandler");
+
+app.use(bodyParser.json());
+app.use("/projects", projectRoutes);
+app.use(errorHandler);
 
 app.use(express.json());// parse the data sent by client in json format
 const cors = require('cors');
 app.use(cors());
 
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log('Server running on port 3001');
-  });
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 const userRouter = require('./routes/users')
