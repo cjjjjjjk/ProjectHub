59a84483a179a4d9c81d99bde508dab24608c8c8
Chu Quang Hưng <137665099+Achootrain@users.noreply.github.com>
2024-10-27 17:18:55 +0700

    fix giao dien, them 1 so logic
    disable button khi  người dùng không nhập đủ thông tin

--- client/src/page/Register.js
+++ client/src/page/Register.js
@@ -10,8 +10,8 @@ function Register() {
   // bien nay de kiem tra xem mk nhap lai co dung ko
   const [showError, setShowError] = useState(false);
 
+ 
   useEffect(() => {
-    console.log(confirmPass);
     if (password === confirmPass) setShowError(false);
     else setShowError(true);
   }, [confirmPass, password]);
@@ -25,10 +25,10 @@ function Register() {
         <h1 className="text-4xl font-bold text-center mb-6">Sign up</h1>
         <form action="" className="grid grid-cols-2 gap-x-4  ">
           {/* Tên người dùng */}
-          <div className=" relative  my-2 p-2 pt-4 border-2 border-black rounded-lg col-span-2">
+          <div className=" relative my-2 p-2 pt-4 border-2 border-black rounded-lg col-span-2">
             <input
               type="text"
-              className="block w-full py-2.3 px-0 text-lg bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              className="block text-lg bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               onChange={(e) => setFullName(e.target.value)}
             ></input>
             <label
@@ -55,10 +55,10 @@ function Register() {
           </div>
 
           {/* User name */}
-          <div className="relative w-72 my-2 p-2 pt-4 border-2 border-black rounded-lg col-span-2">
+          <div className="relative my-2 p-2 pt-4 border-2 border-black rounded-lg col-span-2">
             <input
               type="text"
-              className="block w-full py-2.3 px-0 text-lg bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              className="block  text-lg bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               onChange={(e) => setUsername(e.target.value)}
             ></input>
             <label
@@ -68,39 +68,40 @@ function Register() {
               User name
             </label>
           </div>
+           
+            {/* Password */}
+            <div className="relative my-2 p-2 pt-4 border-2 border-black rounded-lg">
+              <input
+                className="block w-full border-b-2 border-gray-300  dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+              <label
+                htmlFor=""
+                className="absolute text-lg  duration-300 transform -translate-y-5 scale-75  top-4 -z-10 origin-[0] peer-focus:left peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 "
+              >
+                Password
+              </label>
+            </div>
 
-          {/* Password */}
-          <div className="relative w-56 my-2 p-2 pt-4 border-2 border-black rounded-lg">
-            <input
-              className="block w-full py-2.3 px-0 text-lg  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-            <label
-              htmlFor=""
-              className="absolute text-lg  duration-300 transform -translate-y-5 scale-75  top-4 -z-10 origin-[0] peer-focus:left peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 "
+            {/*Confirm Password */}
+
+            <div
+              className={`relative  my-2 p-2 pt-4 border-2 rounded-lg ${
+                showError ? "border-red-600" : "border-black"
+              } `}
             >
-              Password
-            </label>
-          </div>
-
-          {/*Confirm Password */}
-
-          <div
-            className={`relative w-56 my-2 p-2 pt-4 border-2 rounded-lg ${
-              showError ? "border-red-600" : "border-black"
-            } `}
-          >
-            <input
-              className="block w-full py-2.3 px-0 text-lg  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              onChange={(e) => setConfirmPass(e.target.value)}
-            ></input>
-            <label
-              htmlFor=""
-              className="absolute text-lg  duration-300 transform -translate-y-5 scale-75  top-4 -z-10 origin-[0] peer-focus:left peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75"
-            >
-              Confirm password
-            </label>
-          </div>
+              <input
+                className="block w-full py-2.3 px-0 text-lg  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                onChange={(e) => setConfirmPass(e.target.value)}
+              ></input>
+              <label
+                htmlFor=""
+                className="absolute text-lg  duration-300 transform -translate-y-5 scale-75  top-4 -z-10 origin-[0] peer-focus:left peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75"
+              >
+                Confirm password
+              </label>
+            </div>
+          
         </form>
 
         {/* Hien thong bao nhap mk ko dung */}
@@ -112,6 +113,7 @@ function Register() {
 
         {/* Nut tao tk moi */}
         <button
+          disabled={showError||!username||!fullName||!email||!password||!confirmPass}
           type="submit"
           className="w-1/2 mb-4 py-2 text-[18px] mt-6 rounded-lg bg-red-600 text-black hover:bg-black hover:text-white"
         >
