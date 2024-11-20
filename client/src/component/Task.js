b97fc53bca4e1ec8423e0375d10bb5a567367272
cjjjjjjk <ho.vanhai03062004@gmail.com>
2024-11-20 12:08:04 +0700

    fix task propertie's names.

--- client/src/component/Task.js
+++ client/src/component/Task.js
@@ -1,10 +1,10 @@
 import { Draggable } from "react-beautiful-dnd";
-import { Select,Modal,Form,Button,DatePicker,Input,Dropdown } from "antd";
+import { Select, Modal, Form, Button, DatePicker, Input, Dropdown } from "antd";
 import { useState } from "react";
 import { EllipsisOutlined } from "@ant-design/icons";
 
 
-const Task = ({ item, index,deleteTask }) => {
+const Task = ({ item, index, deleteTask }) => {
   const moreOptions = [
     {
       key: "1",
@@ -33,7 +33,7 @@ const Task = ({ item, index,deleteTask }) => {
       ),
     },
   ];
-  
+
   const [openTaskDetail, setOpenTaskDetail] = useState(false);
   const getPriority = (value) => {
     if (value == 1) {
@@ -65,48 +65,48 @@ const Task = ({ item, index,deleteTask }) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-        <div className="p-4 items-start min-h-20 w-64 rounded-lg bg-slate-200">
-          <div className="flex flex-row">  
-            <div className={`text-xs rounded-md pl-1 w-1/3 text-white ${getPriority(item.Priority)}`}>{getPriorityName(item.Priority)}</div>
-            <div className="ml-auto">
-            <Dropdown
-                menu={{
-                  items: moreOptions,
-                }}
-                className="flex justify-center px-2"
-                trigger={["click"]}
-              >
-                <div
-                 
-                  className=" w-10 px-2 "
+          <div className="p-4 items-start min-h-20 w-64 rounded-lg bg-slate-200">
+            <div className="flex flex-row">
+              <div className={`text-xs rounded-md pl-1 w-1/3 text-white ${getPriority(item.priority)}`}>{getPriorityName(item.priority)}</div>
+              <div className="ml-auto">
+                <Dropdown
+                  menu={{
+                    items: moreOptions,
+                  }}
+                  className="flex justify-center px-2"
+                  trigger={["click"]}
                 >
-                  <EllipsisOutlined className="  rounded-lg hover:bg-gray-200 cursor-pointer" />
-                </div>
-              </Dropdown>
+                  <div
+
+                    className=" w-10 px-2 "
+                  >
+                    <EllipsisOutlined className="  rounded-lg hover:bg-gray-200 cursor-pointer" />
+                  </div>
+                </Dropdown>
+              </div>
             </div>
-          </div> 
-          <div className="flex flex-row items-center justify-between gap-x-4">
-            {/* Text */}
-            <div className="overflow-hidden text-ellipsis whitespace-nowrap flex-1 text-sm">
-              <div className="pl-1">{item.name} </div>
+            <div className="flex flex-row items-center justify-between gap-x-4">
+              {/* Text */}
+              <div className="overflow-hidden text-ellipsis whitespace-nowrap flex-1 text-sm">
+                <div className="pl-1">{item.name} </div>
+              </div>
+              {/* Button */}
+
             </div>
-            {/* Button */}
-            
+            <div className="text-xs text-gray-500 pl-1">{item.start_date} - {item.end_date}</div>
           </div>
-          <div className="text-xs text-gray-500 pl-1">{item.Start_date} - {item.End_date}</div>
-        </div>
-        <Modal open={openTaskDetail} 
-          title={item.name}
-          footer={null}
-          onCancel={() => setOpenTaskDetail(false)}
-          width={800} // Set width of the modal
-          > 
-        </Modal>
+          <Modal open={openTaskDetail}
+            title={item.name}
+            footer={null}
+            onCancel={() => setOpenTaskDetail(false)}
+            width={800} // Set width of the modal
+          >
+          </Modal>
         </div>
       )}
     </Draggable>
 
   );
-} 
+}
 
 export default Task;
